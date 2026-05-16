2525b9920b314894b5036d8ddfbb35b83e8e74f2
LA <1245661240@qq.com>
2026-05-16 16:18:42 +0800

    修复按键判定问题

--- osu.Game.Rulesets.Diva/Objects/Drawables/DrawableDivaDoubleHitObject.cs
+++ osu.Game.Rulesets.Diva/Objects/Drawables/DrawableDivaDoubleHitObject.cs
@@ -6,12 +6,14 @@ using osu.Framework.Input.Events;
 
 namespace osu.Game.Rulesets.Diva.Objects.Drawables
 {
+    /// <summary>
+    /// PSP手柄的双键映射，一个note可以使用两个同方向按键之中的任意一个来打击
+    /// </summary>
     public partial class DrawableDivaDoubleHitObject : DrawableDivaHitObject
     {
         private const int max_count = 2;
         private List<DivaAction> inputs = new List<DivaAction>();
         private readonly DivaAction doubleAction;
-        private int inputCount;
 
         public DrawableDivaDoubleHitObject(DivaHitObject hitObject)
             : base(hitObject)
@@ -25,26 +27,39 @@ namespace osu.Game.Rulesets.Diva.Objects.Drawables
         {
             this.Samples.Play();
 
-            if (Judged || inputCount > 2)
+            if (Judged)
                 return false;
 
-            if (e.Action == ValidAction || e.Action == doubleAction)
-                inputCount++;
-
-            if (inputCount == 2)
+            // 方向键 -> 符号键的映射
+            var directionToSymbol = e.Action switch
             {
-                Pressed = true;
-                ValidPress = true;
-            }
+                DivaAction.Right => DivaAction.Circle,
+                DivaAction.Down => DivaAction.Cross,
+                DivaAction.Up => DivaAction.Triangle,
+                DivaAction.Left => DivaAction.Square,
+                _ => e.Action,
+            };
 
-            // inputs.Add(action);
-            // inputCount++;
+            // 符号键 -> 方向键的映射
+            var symbolToDirection = e.Action switch
+            {
+                DivaAction.Circle => DivaAction.Right,
+                DivaAction.Cross => DivaAction.Down,
+                DivaAction.Triangle => DivaAction.Up,
+                DivaAction.Square => DivaAction.Left,
+                _ => e.Action,
+            };
 
-            // if(inputCount == 2)
-            // {
-            //  pressed = true;
-            //  validPress = inputs.Contains(validAction) && inputs.Contains(doubleAction);
-            // }
+            // 检查是否匹配ValidAction或DoubleAction：直接匹配、方向转符号、符号转方向
+            // DoublePressButton只需要按其中一个键就能判定
+            ValidPress = e.Action == ValidAction ||
+                         e.Action == doubleAction ||
+                         directionToSymbol == ValidAction ||
+                         directionToSymbol == doubleAction ||
+                         symbolToDirection == ValidAction ||
+                         symbolToDirection == doubleAction;
+
+            Pressed = true;
 
             return true;
         }
