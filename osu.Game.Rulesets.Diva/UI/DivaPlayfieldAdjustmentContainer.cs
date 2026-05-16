c744867ef19b0d270a5912ae9dabe42206a005d2
LA <1245661240@qq.com>
2026-05-16 22:57:11 +0800

    修改游戏场地基于std

--- osu.Game.Rulesets.Diva/UI/DivaPlayfieldAdjustmentContainer.cs
+++ osu.Game.Rulesets.Diva/UI/DivaPlayfieldAdjustmentContainer.cs
@@ -2,19 +2,19 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Graphics;
-using osu.Game.Rulesets.UI;
-using osuTK;
+using osu.Game.Rulesets.Osu.UI;
 
 namespace osu.Game.Rulesets.Diva.UI
 {
-	public partial class DivaPlayfieldAdjustmentContainer : PlayfieldAdjustmentContainer
+    public partial class DivaPlayfieldAdjustmentContainer : OsuPlayfieldAdjustmentContainer
     {
         public DivaPlayfieldAdjustmentContainer()
         {
             Anchor = Anchor.Centre;
             Origin = Anchor.Centre;
 
-            Size = new Vector2(0.8f);
+            // 使用完整的游戏区域，不进行缩放
+            // Size = new Vector2(0.8f);
         }
     }
 }
